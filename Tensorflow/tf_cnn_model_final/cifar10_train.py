9eb39f8dfe41ed5f497d05498e54a980de3a4255
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2016-12-11 10:39:24 -0600

    Updated TF model to print elapsed time in minutes

--- Tensorflow/tf_cnn_model_final/cifar10_train.py
+++ Tensorflow/tf_cnn_model_final/cifar10_train.py
@@ -95,10 +95,12 @@ def train():
 
     summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
 
+    total_time = 0
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
       _, loss_value = sess.run([train_op, loss])
       duration = time.time() - start_time
+      total_time += duration
 
       assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
@@ -107,9 +109,9 @@ def train():
         examples_per_sec = num_examples_per_step / duration
         sec_per_batch = float(duration)
 
-        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+        format_str = ('%.2f min: step %d, loss = %.2f (%.1f examples/sec; %.3f '
                       'sec/batch)')
-        print (format_str % (datetime.now(), step, loss_value,
+        print (format_str % ((total_time / 60.0), step, loss_value,
                              examples_per_sec, sec_per_batch))
 
       if step % 100 == 0:
