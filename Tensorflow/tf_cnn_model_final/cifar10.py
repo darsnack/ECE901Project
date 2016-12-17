c6da4c93aba0c72fa0a2ccccdc7451bb88602a2e
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2016-12-17 10:50:59 -0600

    Updated learning rate

--- Tensorflow/tf_cnn_model_final/cifar10.py
+++ Tensorflow/tf_cnn_model_final/cifar10.py
@@ -60,7 +60,7 @@ IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE * IMAGE_CHANNELS
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.001       # Initial learning rate.
+INITIAL_LEARNING_RATE = 0.0005       # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
 # to differentiate the operations. Note that this prefix is removed from the
